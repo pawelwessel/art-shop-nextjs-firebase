aea9fd88d1d8cea2075605535dd744720199b4e5
pawelwessel <wesiudev.rekrutacja@gmail.com>
2025-03-19 00:16:31 +0100

    form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,16 @@ import ShopFooter from "@/components/Home/ShopFooter";
 import PrepareCart from "@/components/Home/PrepareCart";
 import Hero from "@/components/Home/Hero";
 import dynamic from "next/dynamic";
-// const Products = dynamic(() => import("@/components/Home/Products"));
+const Products = dynamic(() => import("@/components/Home/Products"));
 export default async function Shop() {
-  // const products: any = await fetch(
-  //   `${process.env.NEXT_PUBLIC_SITE_URL}/api/products`
-  // ).then((res) => res.json());
+  const products: any = await fetch(
+    `${process.env.NEXT_PUBLIC_SITE_URL}/api/products`
+  ).then((res) => res.json());
   return (
     <div className="bg-white flex flex-col justify-center w-full">
       <PrepareCart />
       <Hero />
-      {/* {products?.length > 0 && <Products products={products} />} */}
+      {products?.length > 0 && <Products products={products} />}
       <Orders />
       <ShopFooter />
     </div>
