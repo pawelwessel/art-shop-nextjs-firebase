3c494faf02c58db207ce47c299687ffc014c14c0
pawelwessel <wesiudev.rekrutacja@gmail.com>
2025-03-22 19:42:05 +0100

    revalidation + cache

--- src/app/admin/shop/products/page.tsx
+++ src/app/admin/shop/products/page.tsx
@@ -1,16 +1,9 @@
+import { getDocuments } from "@/firebase";
 import AdminProducts from "./AdminProducts";
 import Ranking from "./Ranking";
 
 export default async function Page() {
-  const products: any = await fetch(
-    `${process.env.NEXT_PUBLIC_SITE_URL}/api/products`,
-    {
-      cache: "no-store",
-      next: {
-        revalidate: 30,
-      },
-    }
-  ).then((res) => res.json());
+  const products: any = await getDocuments("products");
   return (
     <div className="p-6 lg:p-16">
       <h1 className="text-3xl font-cardo text-black">Obrazy na stronie</h1>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,19 +5,10 @@ import PrepareCart from "@/components/Home/PrepareCart";
 import Hero from "@/components/Home/Hero";
 import Products from "@/components/Home/Products";
 import ClientFormWrapper from "@/components/Home/CtaForm/ClientFormWrapper";
-import { addDocument } from "@/firebase";
+import { addDocument, getDocuments } from "@/firebase";
 import { v4 as uuidv4 } from "uuid";
 export default async function Shop() {
-  const products: any = await fetch(
-    `${process.env.NEXT_PUBLIC_SITE_URL}/api/products`,
-    {
-      cache: "no-store",
-      next: {
-        revalidate: 30,
-      },
-    }
-  ).then((res) => res.json());
-
+  const products: any = await getDocuments("products");
   await addDocument("page-views", uuidv4(), {
     page: "shop",
     date: Date.now(),
