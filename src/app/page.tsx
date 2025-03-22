26a3e26d56974e0656c20d73906b06ee2032724c
pawelwessel <wesiudev.rekrutacja@gmail.com>
2025-03-22 19:49:44 +0100

    revalidation + cache

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,18 @@ import PrepareCart from "@/components/Home/PrepareCart";
 import Hero from "@/components/Home/Hero";
 import Products from "@/components/Home/Products";
 import ClientFormWrapper from "@/components/Home/CtaForm/ClientFormWrapper";
-import { addDocument, getDocuments } from "@/firebase";
+import { addDocument } from "@/firebase";
 import { v4 as uuidv4 } from "uuid";
-export default async function Shop() {
-  const products: any = await getDocuments("products");
+export default async function Page() {
+  const products = await fetch(
+    `${process.env.NEXT_PUBLIC_SITE_URL}/api/products`,
+    {
+      cache: "no-store",
+      next: {
+        revalidate: 30,
+      },
+    }
+  ).then((res) => res.json());
   await addDocument("page-views", uuidv4(), {
     page: "shop",
     date: Date.now(),
