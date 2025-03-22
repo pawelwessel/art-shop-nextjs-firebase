38310f5cd67a49d33a44b400015f4e161219270f
pawelwessel <wesiudev.rekrutacja@gmail.com>
2025-03-22 20:26:58 +0100

    /page and /admin fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  experimental: {
+    serverComponentsHmrCache: false, // defaults to true
+  },
   images: {
     remotePatterns: [
       {
