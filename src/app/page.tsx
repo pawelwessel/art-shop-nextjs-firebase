78d92bc66977c16b53d6f23e958182d9210a382b
pawelwessel <wesiudev.rekrutacja@gmail.com>
2025-03-22 20:44:03 +0100

    /page and /admin fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,7 @@ import PrepareCart from "@/components/Home/PrepareCart";
 import Hero from "@/components/Home/Hero";
 import Products from "@/components/Home/Products";
 import ClientFormWrapper from "@/components/Home/CtaForm/ClientFormWrapper";
-import { addDocument } from "@/firebase";
-import { v4 as uuidv4 } from "uuid";
 export default async function Page() {
-  await addDocument("page-views", uuidv4(), { date: Date.now(), page: "shop" });
   return (
     <div className="bg-white flex flex-col justify-center w-full">
       <PrepareCart />
