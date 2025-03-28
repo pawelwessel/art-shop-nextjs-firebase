27386d1cc95e3fcf528a7c1924cf811fb42a83e2
pawelwessel <wesiudev.rekrutacja@gmail.com>
2025-03-28 20:56:09 +0100

    cta

--- src/components/Home/CtaForm/index.tsx
+++ src/components/Home/CtaForm/index.tsx
@@ -1,7 +1,8 @@
 "use client";
+import { addDocument } from "@/firebase";
 import { setModalVisible } from "@/redux/slices/actionSlice";
 import { useDispatch } from "react-redux";
-
+import { v4 as uuid } from "uuid";
 export default function Cta({ label }: { label: string }) {
   const dispatch = useDispatch();
 
@@ -10,7 +11,14 @@ export default function Cta({ label }: { label: string }) {
   }
   return (
     <button
-      onClick={() => setModalVisibility("client")}
+      onClick={() => {
+        const id = uuid();
+        setModalVisibility("client");
+        addDocument("formEngagements", id, {
+          createdAt: new Date(),
+          id,
+        });
+      }}
       className="z-20 bg-black text-white py-3 px-6 shadow-md transition duration-300 hover:bg-gray-800"
     >
       {label}
